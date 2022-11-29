2872a2b201ea97ee4c8f946b0e057ed78634a3e3
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2022-11-29 09:57:50 +0300

    Done

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b){
+    var c = a + b;
+    return c
+}
+function subtract(d, e){
+    var f = d - e;
+    return f
+}
+function multiply(g, h){
+    var i = g * h
+    return i
+}
+function divide(j, k){
+    var l = j / k
+    return l
+}
+function increment(n){
+    var O = n
+    n++
+    return n
+}
+function decrement(n){
+    var p = n
+    n--
+    return n
+}
+function makeInt(n){
+    var q = parseInt(n, 10)
+    return q
+}
+function preserveDecimal(n){
+    var r = parseFloat(n, 10)
+    return r
+}
+
+
